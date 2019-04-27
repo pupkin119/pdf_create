6ac84f256a4e89117f7ac18d1cc95ed524060206
Alex <pupkin11999@gmail.com>
2019-04-27 23:57:25 +0300

    fix all

---
+++ static/js/exel.a6f669243814.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+    $('.tooltipped').tooltip();
+});
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function upload() {
+
+    // if (!($('#town').val())) {
+    //     M.toast({html: 'Заполните город!'});
+    //     $('#progress_manual').hide();
+    //     return;
+    // }
+
+    $('.progress').show();
+    $('#progress_manual_text').show();
+    var fd = new FormData();
+    var files = $('#file')[0].files[0];
+    fd.append('file', files);
+    // fd.append('town', $('#town').val());
+
+// $.ajax({
+//     csrfmiddlewaretoken: getCookie('csrftoken'),
+//     url: $(this).attr('action'),
+//     type: $(this).attr('method'),
+//     data: fd,
+//     // data: $("#registerSubmit").serialize(),
+//     cache: false,
+//     processData: false,
+//     contentType: false,
+//     success: function(data) {
+//         alert('success');
+//     }
+// }); 
+// location = window.location.hostname;
+// var new_url = location + '/preview/';
+
+    var href = '/upload_exel';
+    $.ajax({
+        type: "post",
+        url: href,
+        async: true,
+        data: fd,
+        cache: false,
+        processData: false,
+        contentType: false,
+        success: function (data) {
+            $('#progress').hide();
+            $('#progress_manual_text').hide();
+            // alert('yes');
+            window.location = /preview/ + data['success'];
+
+        }
+
+    });
+
+    return false;
+}
